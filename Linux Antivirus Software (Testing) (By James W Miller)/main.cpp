ba53ae560fdca2b0d977d88d13979ba1b75ec9c6
JWMiller006 <95261008+JWMiller006@users.noreply.github.com>
2021-11-29 20:33:43 -0500

    Add files via upload

---
+++ Linux Antivirus Software (Testing) (By James W Miller)/main.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include<string>
+int cfViruses()
+{
+    printf("Checking File access and restrictions...");
+    checkfiles();
+        //if ();
+    return 0;
+}
+int checkfiles()
+{
+    bool find_file(const path & dir_path,         // in this directory,
+        const std::string & file_name, // search for this name,
+        path & path_found)            // placing path here if found
+    {
+        if (!exists(dir_path)) return false;
+        directory_iterator end_itr; // default construction yields past-the-end
+        for (directory_iterator itr(dir_path);
+            itr != end_itr;
+            ++itr)
+        {
+            if (is_directory(itr->status()))
+            {
+                if (find_file(itr->path(), file_name, path_found)) return true;
+            }
+            else if (itr->leaf() == file_name) // see below
+            {
+                path_found = itr->path();
+                return true;
+            }
+        }
+        return false;
+    }
+}
+int main()
+{
+    printf("Hello There!");
+    return 0;
+    cfViruses();
+}
